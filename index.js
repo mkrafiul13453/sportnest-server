92dd11547e00e72c100c3a8db833714e517fe242
mkrafiul13453 <mkrafiul13453@gamil.com>
2026-06-14 21:45:52 +0600

    Booking API is created

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 require('dotenv').config()
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors')
+app.use(cors())
+app.use(express.json())
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = process.env.MONGO_URI
 const port = process.env.PORT
 
@@ -16,6 +19,82 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
+        const db = client.db('sportnest')
+        const facilityCollection = db.collection('facilities')
+        const bookingCollection = db.collection('bookings')
+
+
+        // ************************************************Creating the booking post API***********************************************************
+        app.post('/booking', async (req, res) => {
+            const bookingData = req.body
+            const result = await bookingCollection.insertOne(bookingData)
+            res.send(result)
+        })
+
+
+        // ************************************************Creating the booking get API***********************************************************
+        app.get('/booking/:userId', async (req, res) => {
+            const userId = req.params.userId
+            const query = { userId: userId }
+            const result = await bookingCollection.find(query).toArray()
+            res.send(result)
+        })
+
+
+        // ************************************************Creating the booking delete API***********************************************************
+        app.delete('/booking/:bookingId', async (req, res) => {
+            const id = req.params.bookingId
+            const query = { _id: new ObjectId(id) }
+            const result = await bookingCollection.deleteOne(query)
+            res.send(result)
+        })
+
+
+        // ************************************************Creating the facility post API***********************************************************
+        app.post('/facility', async (req, res) => {
+            const facilityData = req.body
+            const result = await facilityCollection.insertOne(facilityData)
+            res.send(result)
+        })
+
+
+        // ************************************************Creating the facility get API***********************************************************
+        app.get('/facility', async (req, res) => {
+            const result = await facilityCollection.find().toArray()
+            res.send(result)
+        })
+
+
+        // ************************************************Creating the facility get API by ID***********************************************************
+        app.get('/facility/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await facilityCollection.findOne(query)
+            res.send(result)
+        })
+
+
+
+        // ************************************************Creating the facility Patch API by ID***********************************************************
+        app.patch('/facility/:id', async (req, res) => {
+            const id = req.params.id
+            const updateData = req.body
+            const query = { _id: new ObjectId(id) }
+            const result = await facilityCollection.updateOne(query, { $set: updateData })
+            res.send(result)
+        })
+
+
+        // ************************************************Creating the facility Delete API by ID***********************************************************
+        app.delete('/facility/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await facilityCollection.deleteOne(query)
+            res.send(result)
+        })
+
+
+    
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         // Send a ping to confirm a successful connection
